4f895cc8b2366000c4e1d9362b268027d3009093
olgakuzminagithub <olga.kuzmina1989@gmail.com>
2018-09-10 14:35:37 +0300

    classwork

---
+++ classwork/classwork-4/web-store/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <div>
+            <Link/>
+            <Route/>
+          </div>
+        </BrowserRouter>
+    )
+
+  }
+}
+
+export default App;
